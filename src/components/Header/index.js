acc837fe9b60651bb50aadc14ce67f1d47cbcc5d
san man <sance1921@gmail.com>
2021-12-19 16:42:41 +0100

    correct component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 // == Import NPM
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 // == Import library @material-ui
 import { makeStyles } from '@material-ui/core/styles';
 import { 
@@ -8,8 +9,7 @@ import {
   Button,
 }from '@material-ui/core';
 // == Import image
-import Logo from '../Header/logo_azap.png';
-
+import Logo from '../../images/logo_azap.png';
 /** variable useStyles store function makeStyles
  * function makeStyles () store component's classes uses for styling
  */
@@ -27,25 +27,47 @@ const useStyles = makeStyles(() => ({
   },
   button: {
     backgroundColor: '#F8D800',
+    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
   },
 }));
 
 /** function Header export Header component  */
-export default function Header() {
+const Header = ({ isLogged,setIsLogged }) => {
   const classes = useStyles();
 
+  const logOut = () => {
+    localStorage.clear();
+    setIsLogged(false);
+  };
+  
   return (
     <div className={classes.root}>
+ 
       <AppBar position="static">
         <Toolbar className={classes.headerContainer}>
-          <Button href="/">
-            <img src={Logo} alt="Logo" className={classes.logo}/>
-          </Button>    
-          <Button variant="contained" href="/sign-in" className={classes.button} >
-            Connexion
-          </Button>
+
+        <Button component={Link} to="/">
+          <img src={Logo} alt="Logo" className={classes.logo}/>
+        </Button> 
+
+        {!isLogged && ( 
+          <Button variant="contained" component={Link} to="/sign-in" className={classes.button} 
+          >
+            connexion
+          </Button> 
+        )}
+
+        {isLogged && ( 
+          <Button variant="contained" component={Link} to="/" className={classes.button} 
+          onClick={logOut}
+          >
+          deconnexion
+        </Button> 
+        )}
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+export default Header;

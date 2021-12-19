95a3348e4356df2db0d9395b2d05b000fc077e4a
san man <sance1921@gmail.com>
2021-12-19 16:53:05 +0100

    correct component search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,82 +1,72 @@
 // == Import NPM
-import React from "react";
-import { useState } from "react";
-//import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 // == Import components
-import Header from "../Header/index";
 import NavBar from "../NavBar";
 import InputSearch from "../InputSearch";
+import DisplaySearch from "../DisplaySearch";
 import Filters from "../Filters";
-import DisplaySearch from "../DisplaySearch/";
-import Footer from "../Footer";
-// import api from 'src/api';
 
+/** permet de faire une recherche par nom, realisateur à partir de l'Api */
 const Search = () => {
-    const [searchText, setsearchText] = useState("");
-    const [resultat, setResultat] = useState([]);
+  const [searchText, setsearchText] = useState("");
+  const [resultat, setResultat] = useState([]);
 
-    const handleChange = (textSaisi) => {
-        setsearchText(textSaisi);
-        // console.log('texteSaisie', textSaisi);
-    };
-    // à supprimer pour sécuriser le code
-    //const api_key='455dc6a8d89a57de7742f24ebbf4b441';
-    const fetchResult = async () => {
-        //console.log('mooooooiii')
-        // J'utilise axios pour lancer la requête
-
-        /********** DEBUT : code ajouté par Back *************/
-        try {
-            const response = await fetch(
-                `http://localhost:3050/v1/search/${searchText}`
-            );
-            const data = await response.json();
-            console.log("🚀 ~ data", data);
-            setResultat(data);
-        } catch (err) {
-            console.log("🚀 ~ err", err);
-        }
-        /********** FIN : code ajouté par Back *************/
-
-        // axios({
-        //     method: "get",
-        //     //url:` https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=fr&query=${searchText}`
-        //     url: `http://localhost:3050/v1/search`,
-        // })
-        //     .then((res) => {
-        //         // Dans res.data mon objet avec la list des films
-        //         setResultat(res.data.results);
-        //         //console.log('data==========================>',res.data)
-        //     })
-        //     // Si j'ai les résultats
-        //     // faire un console.log(res.data)
-        //     // Gérer l'affichage des résultats
-        //     // -> mettre le tableau de repos dans le state
-        //     .catch((err) => {
-        //         // Sinon log l'erreur
-        //         console.log(err);
-        //     })
-        //     .finally(() => {});
-    };
-
-    const handleSubmit = () => {
-        // Je veux lancer une requête sur l'API Github
-        fetchResult();
-    };
-    return (
-        <div className="Search">
-            <Header />
-            <NavBar />
-            <InputSearch
-                searchText={searchText}
-                onSearchChange={handleChange}
-                onSearchSubmit={handleSubmit}
-            />
-            <Filters />
-            <DisplaySearch films={resultat} />
-            <Footer />
-        </div>
+  /** permet de recuperer le texte saisie par l'utilisateur */
+  const handleChange = (textSaisi) => {
+    setsearchText(textSaisi);
+    console.log(
+      "verif data from component Search => handleChange :",
+      textSaisi
     );
+  };
+  /** fetch vers l'Api pour recuperer les resultat de la recherche*/
+  const fetchResult = async () => {
+    try {
+      // en premier je vide la mémoire de l'ancienne recherche
+      localStorage.removeItem("lastSearchedMovies");
+
+      // envoie de la requete vers API AZAP
+      const response = await axios.get(
+        `https://projet-azap-heroku.herokuapp.com/v1/search/${searchText}`
+      );
+
+      // si success requete retourne un tableau de movies
+      const movies = response.data;
+
+      // on passe le tableau de movies dans le state pour affichage dans le composant displaySearch
+      setResultat(movies);
+
+      //on stocke le tableau des movies dans le localStorage afin de garder en mémoire le résulat
+      localStorage.setItem("lastSearchedMovies", JSON.stringify(movies));
+
+      // permet de vider la derniere recherche enregistrée au bout de 5 minutes
+      setTimeout(function () {
+        localStorage.removeItem("lastSearchedMovies");
+      }, 5 * 60 * 1000);
+      
+    } catch (error) {
+      console.error("error", error.message);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Je veux lancer une requête sur l'API Github
+    fetchResult();
+  };
+
+  return (
+    <div className="Search">
+      <NavBar />
+      <InputSearch
+        searchText={searchText}
+        onSearchChange={handleChange}
+        onSearchSubmit={handleSubmit}
+      />
+      <Filters setResultat={setResultat} />
+      <DisplaySearch films={resultat} />
+    </div>
+  );
 };
 
 export default Search;
